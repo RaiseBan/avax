51954d3764ff3faa8b84a64232235863871e639c
RaiseBan <shornikov1407@gmail.com>
2023-12-01 14:19:40 +0300

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Main {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.gecko.driver", "C:\\geckodriver.exe");
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://avax.hyperspace.xyz/collection/avax/2b052ded-2ee6-4005-86d9-c14e5cd609ad");
+        Logic logic = new Logic(driver);
+        logic.getActualPrice();
+        logic.getInstantlyPrice();
+
+
+
+
+
+
+
+
+
+
+    }
 }
